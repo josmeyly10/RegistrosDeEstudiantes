60a21da03ad44ea2cdc3b70904f6cbf26bfd0577
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2026-01-31 16:54:53 -0400

    Cambie un numero de una version que tenia mal

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
+
 }
